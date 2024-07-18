30ef9317f0e8bc1fea701415dc420ad12352882b
prinzprogrammer <princeprogrammer73@gmail.com>
2024-07-18 23:48:38 +0800

    add pokemon game route

--- pokemonCollection/src/App.jsx
+++ pokemonCollection/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Pages/Home/Home.jsx";
 import About from "./Pages/About/About.jsx";
 import Contact from "./Pages/Contact/Contact.jsx";
 import PokemonDetails from "./Pages/PokemonDetails/PokemonDetails.jsx";
+import PokemonGame from "./Pages/PokemonGame/PokemonGame.jsx";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/pokemon/:pokemonId" element={<PokemonDetails />} />
+      <Route path="/pokemon-game" element={<PokemonGame />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<h1>This page do not exists</h1>} />
