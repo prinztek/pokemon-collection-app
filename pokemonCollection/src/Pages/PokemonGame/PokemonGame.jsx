f2815f8537589947d61478edab665d28e7dc57dc
prinzprogrammer <princeprogrammer73@gmail.com>
2024-07-19 01:01:31 +0800

    add functionality in pokemon game page

--- pokemonCollection/src/Pages/PokemonGame/PokemonGame.jsx
+++ pokemonCollection/src/Pages/PokemonGame/PokemonGame.jsx
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getPokemon, upperCaseFirtLetter, getRandomNumber } from '../../Utility/utils.js';
 import BarLoader from "react-spinners/BarLoader";
 import "./PokemonGame.css";
@@ -7,11 +7,15 @@ import "./PokemonGame.css";
 const PokemonGame = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [pokemon, setPokemon] = useState();
+  const [input, setinput] = useState();
+  const imageRef = useRef();
+
 
   useEffect(() => {
     setIsLoading(true);
     async function handleRandomPokemon() {
-      const pokemon = await getPokemon(getRandomNumber(1, 10));
+      // should be a fetch request
+      const pokemon = await getPokemon(getRandomNumber(1, 1025));
       setPokemon(pokemon); //
       setIsLoading(false);
       console.log(pokemon.name);
@@ -19,25 +23,69 @@ const PokemonGame = () => {
     handleRandomPokemon(setPokemon);
   }, [])
 
-  async function getAnotherPokemon() {}
+  function onInputChange(e) {
+    const target = e.target.value;
+    setinput(target);
+  }
+
+  async function getAnotherPokemon() {
+    setIsLoading(true);
+    const pokemon = await getPokemon(getRandomNumber(1, 1025));
+    setPokemon(pokemon); //
+    setIsLoading(false);
+  }
+
+  function showImage() {
+    let value = imageRef.current.style.filter;
+    if (value === "") {
+      imageRef.current.style.filter = "brightness(1)";
+    } else {
+      imageRef.current.style.filter = "";
+    }
+  }
+
+  function submitName() {
+    const userInput = input.toLowerCase();
+    const pokemonName = pokemon.name.toLowerCase();
+    if (pokemonName === userInput) {
+      imageRef.current.style.filter = "brightness(1)";
+      setTimeout(() => {
+        getAnotherPokemon();
+      }, 5000);
+    } else {
+      alert("Wrong Answer!!!");
+      console.log(pokemonName);
+    }
+  }
 
   return (
-    <div className="pokemon-game">
-      { isLoading ? (
-          <BarLoader />
-        ) : (
-          <>
-            { pokemon && (
-                <div className="pokemon-game-image-container">
-                  <img
-                    id="silhouette"
-                    src={pokemon.sprites.other["official-artwork"].front_default}
-                  />
-                </div>
-            )}
-          </>
-      )}
-    </div>
+    <main>
+      <div id="pokemon-game-container">
+        { isLoading ? (
+            <BarLoader />
+          ) : (
+            <>
+              { pokemon && (
+                <>
+                  <h1>Who's That Pokemon?</h1>
+                  <div className="pokemon-game-image-container">
+                    <img
+                      ref={imageRef}
+                      id="silhouette"
+                      src={pokemon.sprites.other["official-artwork"].front_default}
+                    />
+                  </div>
+                  <div className="game-btns">
+                    <button className="btn" onClick={showImage} >Show</button>
+                    <input onChange={onInputChange} id="silhouette-input" type="text" placeholder="Enter the Pokemon name" />
+                    <button className="btn" onClick={submitName} >Submit</button>
+                  </div>
+                </>
+              )}
+            </>
+        )}
+      </div>
+    </main>
   );
 
 };
