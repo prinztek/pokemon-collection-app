0e7b3eb78c8365a2beaf43829b1b23f1d990f272
prinzprogrammer <princeprogrammer73@gmail.com>
2024-07-30 23:49:23 +0800

    add who's that pokemon audio with mute and unmute function button that also display the current state of audio

--- pokemonCollection/src/Pages/PokemonGame/PokemonGame.jsx
+++ pokemonCollection/src/Pages/PokemonGame/PokemonGame.jsx
@@ -3,19 +3,31 @@ import { useEffect, useState, useRef } from 'react';
 import { getPokemon, upperCaseFirtLetter, getRandomNumber } from '../../Utility/utils.js';
 import ClipLoader from "react-spinners/ClipLoader";
 import "./PokemonGame.css";
+import pokemonAudio from '../../assets/pokemon-audio.mp3'; // Import the audio file
 
 const PokemonGame = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [pokemon, setPokemon] = useState();
   const [input, setinput] = useState();
+  const [isMuted, setIsMuted] = useState(false);
   const [resultText, setResultText] = useState("Who's That Pokemon?");
   const imageRef = useRef();
-
+  const audioRef = useRef(new Audio(pokemonAudio)); // Create a ref for the audio element
 
   useEffect(() => {
-    setIsLoading(true);
     handleRandomPokemon();
   }, [])
+
+  function playAudio() {
+    audioRef.current.play();
+  }
+
+  function muteAudio() {
+    if (audioRef.current) {
+      audioRef.current.muted = !audioRef.current.muted;
+      setIsMuted((prevState) => !prevState);
+    } 
+  }
 
   function onInputChange(e) {
     const target = e.target.value;
@@ -27,6 +39,7 @@ const PokemonGame = () => {
     const pokemon = await getPokemon(getRandomNumber(1, 493));
     setPokemon(pokemon); 
     setIsLoading(false);
+    playAudio();
   }
 
   function showImage() {
@@ -38,7 +51,11 @@ const PokemonGame = () => {
     }
   }
 
-  function submitName() {
+  function submitName(e) {
+    if (!input) {
+      alert("Pokemon name cannot be blank!!!");
+      return
+    }
     const userInput = input.toLowerCase();
     const pokemonName = pokemon.name.toLowerCase();
     if (pokemonName === userInput) {
@@ -48,6 +65,7 @@ const PokemonGame = () => {
         handleRandomPokemon();
         setResultText("Who's That Pokemon?");
       }, 3000);
+      return
     } else {
       alert("Wrong Answer!!!");
       console.log(pokemonName);
@@ -70,6 +88,7 @@ const PokemonGame = () => {
   return (
     <main>
       <div id="pokemon-game-container">
+      <audio ref={audioRef} src={pokemonAudio}></audio>
         { isLoading ? (
             <>
               <ClipLoader color={"#0000FF"} speedMultiplier={1.2}/>
@@ -90,8 +109,14 @@ const PokemonGame = () => {
                   <div className="game-controls">
                     <input onKeyDown={onEnterDown} onChange={onInputChange} id="silhouette-input" type="text" placeholder="Enter the Pokemon name" />
                     <div className="game-btns">
-                      <button className="btn" onClick={showImage} >Show</button>
-                      <button className="btn" onClick={submitName} >Submit</button>
+                      <button className="btn" onClick={showImage}>Show</button>
+                      <button className="btn" onClick={submitName}>Submit</button>
+                      { !isMuted ? (
+                          <button className="btn" onClick={muteAudio}>Mute</button>
+                        ) : (
+                          <button className="btn" onClick={muteAudio}>Unmute</button>
+                        )
+                      }
                     </div>
                   </div>
                 </>
