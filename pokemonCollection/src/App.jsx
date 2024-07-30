617b0cfd0c09ba14277a27a1e44054ab61a96d55
prinzprogrammer <princeprogrammer73@gmail.com>
2024-07-30 23:25:12 +0800

    use pokemon context provider component

--- pokemonCollection/src/App.jsx
+++ pokemonCollection/src/App.jsx
@@ -5,21 +5,23 @@ import About from "./Pages/About/About.jsx";
 import Contact from "./Pages/Contact/Contact.jsx";
 import PokemonDetails from "./Pages/PokemonDetails/PokemonDetails.jsx";
 import PokemonGame from "./Pages/PokemonGame/PokemonGame.jsx";
+import PokemonProvider from "./PokemonProvider.jsx";
 import { Route, Routes } from "react-router-dom";
+import { useContext } from 'react';
 
 function App() {
   return (
-    <>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pokemon/:pokemonId" element={<PokemonDetails />} />
-      <Route path="/pokemon-game" element={<PokemonGame />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<h1>This page do not exists</h1>} />
-    </Routes>
-  </>
+    <PokemonProvider>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:pokemonId" element={<PokemonDetails />} />
+        <Route path="/pokemon-game" element={<PokemonGame />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h1>This page do not exists</h1>} />
+      </Routes>
+    </PokemonProvider>
   );
 }
 
